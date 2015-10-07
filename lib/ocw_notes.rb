e04f3d7379e7c2b7b2f255a4bf7740ad5ffc3292
Richo <richo2192000@gmail.com>
2015-10-07 19:57:35 +0800

    Add comments for each of the methods

--- lib/ocw_notes.rb
+++ lib/ocw_notes.rb
@@ -4,18 +4,22 @@ module OcwNotes
   # Url of the source website: http://ocw.nthu.edu.tw/ocw/index.php?page=mediaList&classid=5
   class OpenCourseWareNotes
 
+    # Initialize instance when created
     def initialize(coursename)
     end
 
+    # Return notes in hash
     def notes
         @notes ||= extract_notes
     end
 
     private
 
+    # Parse html from url with given course name
     def parse_html(coursename)
     end
 
+    # Extrace notes from parsed html file
     def extract_notes
     end
 
