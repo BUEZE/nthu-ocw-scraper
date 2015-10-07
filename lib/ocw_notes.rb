9547a75dadc815184d1b34e98c7a3ddd0117e911
Richo <richo2192000@gmail.com>
2015-10-07 19:55:14 +0800

    Defining methods for class OpenCourseWareNotes:
    + initialize
    + notes: return notes in hash
    + parse_html: parse html from specified url
    + extract_notes: extract notes from parsed html

--- lib/ocw_notes.rb
+++ lib/ocw_notes.rb
@@ -1,6 +1,23 @@
 module OcwNotes
   # This class get a user-specified course name as input
   # return a hash of course's notes provided by other students
+  # Url of the source website: http://ocw.nthu.edu.tw/ocw/index.php?page=mediaList&classid=5
   class OpenCourseWareNotes
+
+    def initialize(coursename)
+    end
+
+    def notes
+        @notes ||= extract_notes
+    end
+
+    private
+
+    def parse_html(coursename)
+    end
+
+    def extract_notes
+    end
+
   end
 end
