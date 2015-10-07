74a43e0598b70de866ab8b341e12ac65fb485542
Richo <richo2192000@gmail.com>
2015-10-08 00:51:54 +0800

    Make the code pass Rubocop.

--- spec/notes_spec.rb
+++ spec/notes_spec.rb
@@ -3,67 +3,67 @@ require 'minitest/rg'
 require './lib/ocw_notes.rb'
 
 notes = {
-  '615' => { 
+  '615' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=615',
     'course' => '心理學與現代生活',
     'title' => 'W5記憶',
     'author' => '中文17 陳懷瑜'
   },
-  '614' => { 
+  '614' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=614',
     'course' => '心理學與現代生活',
     'title' => 'W4意識',
     'author' => '詹雨臻'
   },
-  '622' => { 
+  '622' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=622',
     'course' => '心理學與現代生活',
     'title' => 'W14心理疾病的類型與診斷',
     'author' => '中文17 陳懷瑜'
   },
-  '616' => { 
+  '616' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=616',
     'course' => '心理學與現代生活',
     'title' => 'W6學習與制約',
     'author' => '中文17 陳懷瑜'
   },
-  '612' => { 
+  '612' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=612',
     'course' => '心理學與現代生活',
     'title' => 'W2心理學的生物基礎',
     'author' => '詹雨臻'
   },
-  '621' => { 
+  '621' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=621',
     'course' => '心理學與現代生活',
     'title' => 'W13人格',
     'author' => '中文17 陳懷瑜'
   },
-  '620' => { 
+  '620' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=620',
     'course' => '心理學與現代生活',
     'title' => 'W9社會影響與社會認知',
     'author' => '中文17 陳懷瑜'
   },
-  '619' => { 
+  '619' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=619',
     'course' => '心理學與現代生活',
     'title' => 'W10 思考與創造力',
     'author' => '中文17 陳懷瑜'
   },
-  '613' => { 
+  '613' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=613',
     'course' => '心理學與現代生活',
     'title' => 'W3知覺與注意力',
     'author' => '詹雨臻'
   },
-  '617' => { 
+  '617' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=617',
     'course' => '心理學與現代生活',
     'title' => 'W8心理發展',
     'author' => '中文17 陳懷瑜'
   },
-  '618' => { 
+  '618' => {
     'url' => 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=618',
     'course' => '心理學與現代生活',
     'title' => 'W11情緒',
@@ -71,17 +71,12 @@ notes = {
   }
 }
 
-
 describe 'Get all the notes with specific keywords' do
-
   before do
     @notes_found = OcwNotes::OpenCourseWareNotes.get_notes('心理學')
   end
 
   it 'has the right number of notes' do
-    @notes_found.size.must_equal notes.size 
+    @notes_found.size.must_equal notes.size
   end
-
-  
-
 end
