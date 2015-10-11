01da8b30239023edbe37a36b0d656f77aa4c8516
Hiraku <dev@hiraku.tw>
2015-10-11 21:42:11 +0800

    make it pass the spec test

--- lib/open_course_ware_notes.rb
+++ lib/open_course_ware_notes.rb
@@ -6,31 +6,55 @@ require_relative 'ocw_notes'
 class OpenCourseWareNotes
   include OcwNotes
 
+  attr_accessor :course
+  attr_accessor :data
   $home_string = 'http://ocw.nthu.edu.tw/ocw/'
 
   # Parse html from url with given course name
   def parse_html(coursename)
+    data = Hash.new {}
     search_link_str = 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaList&classid=5&keyword='
-    docs = get_url_html(search_link_str + coursename) # get url's html source code
-    search_arr = docs.xpath("//div[@class='singleMediaBlock']") # get all link class by 'singleMediaBlock'
+
+    # get url's html source code
+    docs = get_url_html(search_link_str + coursename)
+
+    # get all link class by 'singleMediaBlock'
+    search_arr = docs.xpath("//div[@class='singleMediaBlock']")
     links = [] # get all search result link
     search_arr.each do |x|
-      links.push x.attributes['onclick'].content.match(/'([^']+)/)[1] # parse onclick string
+      # parse onclick string
+      links.push x.attributes['onclick'].content.match(/'([^']+)/)[1]
     end
-    extract_notes(links[0])
+    links.each do |link|
+      arr = link.split('=')
+      id = arr[arr.length - 1]
+      data[id] = extract_notes(link)
+    end
+    data
   end
 
   # Extrace notes from parsed html file
   def extract_notes(url)
+    @course = Hash.new {}
     target_link = $home_string + url
     # target_link = 'http://ocw.nthu.edu.tw/ocw/index.php?page=mediaContent&id=615'
 
     docs = get_url_html(target_link) # get url's html source code
     # write_to_file(docs, './test.html')
     img_arr = docs.xpath("//div[@class='mediaBlock']/ul/li/img")
+    img_arr2 = []
     img_arr.each do |x|
-      x = $home_string + x.attributes['src'].content
+      img_arr2.push($home_string + x.attributes['src'].content)
     end
-    img_arr
+    title_arr = docs.xpath("//div[@class='post_content_top-bgbtm']/div")
+    title = title_arr[2].xpath('//div/div/h2')
+    course_name = title_arr[2].xpath('//div/div/h3')
+    course_name = course_name.first.content.split(' / ')
+    course['title'] = title[0].content.gsub(/\s+/, '')
+    course['course'] = course_name[0].gsub(/\s+/, '')
+    course['author'] = course_name[1]
+    course['url'] = target_link
+    course['images'] = img_arr2
+    course
   end
 end
